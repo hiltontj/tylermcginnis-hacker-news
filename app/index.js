eb89caac6765b492893c58ea6da1649d7b0a2c12
hiltontj <trevor.hilton@gmail.com>
2019-09-06 17:43:11 -0400

    add in empty components and create the theme context

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+import { ThemeProvider } from './contexts/theme'
+
+export default class App extends React.Component {
+   state = {
+      theme: 'light'
+   }
+
+   render() {
+
+   }
+}
