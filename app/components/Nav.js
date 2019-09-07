37e02fc3522b9df2e4bd2805c692c1f1cde66f25
hiltontj <trevor.hilton@gmail.com>
2019-09-07 12:52:36 -0400

    add shell of loading component, and get the nav and basic router set up

---
+++ app/components/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ThemeConsumer } from '../contexts/theme'
+import { NavLink } from 'react-router-dom'
+
+const activeStyle = {
+   color: "#32a852"
+}
+
+export default function Nav () {
+   return (
+      <ThemeConsumer>
+         {({ theme, toggleTheme }) => (
+            <nav className="row space-between">
+               <ul className="row nav">
+                  <li>
+                     <NavLink
+                        to="/"
+                        exact
+                        activeStyle={activeStyle}
+                        className="nav-link"
+                     >
+                        Top
+                     </NavLink>
+                  </li>
+                  <li>
+                     <NavLink
+                        to="/new"
+                        exact
+                        activeStyle={activeStyle}
+                        className="nav-link"
+                     >
+                        New
+                     </NavLink>
+                  </li>
+               </ul>
+            </nav>
+         )}
+      </ThemeConsumer>
+   )
+}
--- app/index.js
+++ app/index.js
@@ -1,14 +1,47 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 import './index.css'
+import Nav from './components/Nav'
 import { ThemeProvider } from './contexts/theme'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-export default class App extends React.Component {
+// Include Lazy Routes:
+const Posts    = React.lazy(() => import('./components/Posts.js'))
+const User     = React.lazy(() => import('./components/User.js'))
+const Comments = React.lazy(() => import('./components/Comments.js'))
+
+class App extends React.Component {
    state = {
-      theme: 'light'
+      theme: 'light',
+      toggleTheme: () => {
+         this.setState({
+            theme : theme === 'dark' ? 'light' : 'dark'
+         })
+      }
    }
 
    render() {
+      return (
+         <Router>
+            <ThemeProvider value={this.state}>
+               <div className={this.state.theme}>
+                  <div className="container">
+                     <Nav />
 
+                     <React.Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                           <Route render={() => (<h1>404 Not Found.</h1>)} />
+                        </Switch>
+                     </React.Suspense>
+                  </div>
+               </div>
+            </ThemeProvider>
+         </Router>
+      )
    }
 }
+
+ReactDOM.render(
+   <App />,
+   document.getElementById('app')
+)
