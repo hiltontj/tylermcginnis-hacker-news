d64df2a0f1bcc4d06544a1baa9eb7fb958e210d4
hiltontj <trevor.hilton@gmail.com>
2019-09-30 22:44:40 -0400

    refactor Stories to use hooks

--- app/components/Stories.js
+++ app/components/Stories.js
@@ -4,53 +4,24 @@ import { fetchMainPosts } from '../utils/hacker-news-api'
 import Posts from './Posts'
 import Loading from './Loading';
 
-export default class Stories extends React.Component {
-   state = {
-      posts : [],
-      error: null // todo - currently not used
-   }
+export default function Stories({ type }) {
+   const [posts, setPosts] = React.useState(null)
 
-   componentDidMount() {
-      const { type } = this.props
-      this.updatePosts(type)
-   }
+   React.useEffect(() => {
+      setPosts(null)
+      fetchMainPosts(type)
+         .then((result) => setPosts(result))
+   }, [type])
 
-   componentDidUpdate(prevProps) {
-      const { type } = this.props
-      if ( prevProps.type !== type ) {
-         this.setState({
-            posts : []
-         })
-         this.updatePosts(type)
-      }
-   }
-
-   updatePosts = (storyType) => {
-      fetchMainPosts(storyType)
-         .then((result) => {
-            this.setState(({posts}) => ({
-               posts : posts.concat(result)
-            }))
-         })
-   }
-
-   isLoading = () => {
-      const { posts, error } = this.state
-      return posts.length === 0 || error !== null
-   }
-
-   render() {
-      const { posts } = this.state
-      return (
-         <React.Fragment>
-            {
-               this.isLoading() 
-                  ? <Loading text="Loading Stories" />
-                  : <Posts posts={posts} />
-            }
-         </React.Fragment>
-      )
-   }
+   return (
+      <React.Fragment>
+         {
+            posts === null
+               ? <Loading text="Loading Stories" />
+               : <Posts posts={posts} />
+         }
+      </React.Fragment>
+   )
 }
 
 Stories.propTypes = {
